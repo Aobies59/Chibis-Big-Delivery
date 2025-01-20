bdc3d83c1de7d478ab51b3c11e9c97f12aafda0b
Aobies59 <alvaroobiesgarcia@proton.me>
2025-01-20 21:38:01 +0100

    improved camera system

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float positionSpeed = 10f;
+    public float rotationSpeed = 100f;
+
+    public Vector3 offset;
+    public float maxDistance = 10f;
+
+    Vector3 previousTargetPosition;
+
+    void Start()
+    {
+        previousTargetPosition = target.position;
+    }
+
+    void Update()
+    {   
+        Vector3 desiredPosition = target.position;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, positionSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, desiredPosition) > maxDistance) {
+            Vector3 offset = desiredPosition - previousTargetPosition;
+            transform.position += offset;
+        } else {
+            transform.position = smoothedPosition;
+        }
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, target.rotation, rotationSpeed * Time.deltaTime);
+
+        previousTargetPosition = target.position;
+    }
+}
